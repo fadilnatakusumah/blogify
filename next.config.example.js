6b97f0a4a82448738aedafd4a3e5363ee655a8b6
fadilnatakusumah <fadil.ntksmh@gmail.com>
2021-02-12 22:54:15 +0700

    initial commit for blogify-frontend

---
+++ next.config.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  publicRuntimeConfig: {
+    APP_NAME: "Blogify",
+    API_DEV: "http://localhost:8080/api",
+    PRODUCTION: false,
+  },
+  compilerOptions: {
+    "baseUrl": ".",
+  }
+}
